dcec978817031d24beaf6939528db7944244d5f1
Kabir Singh <kabirsingh12b@gmail.com>
2026-04-07 15:26:00 +0530

    feat : created a global Zustand store
    Stores global login state and functions to log user in and out

---
+++ src/store.js
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+
+export const useStore = create((set) => ({
+  isLoggedin: !!localStorage.getItem("user"),
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  login: (userData) => {
+    if (!userData) return;
+    localStorage.setItem("user", JSON.stringify(userData));
+    set(() => ({ isLoggedin: true, user: userData }));
+  },
+  logout: () => {
+    localStorage.removeItem("user");
+    set(() => ({ isLoggedin: false }));
+  },
+}));
