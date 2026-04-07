793d843e0ce16e513213a831f8b93d2ca6836f74
Kabir Singh <kabirsingh12b@gmail.com>
2026-04-07 12:45:37 +0530

    Implemented name modal
    Conditonally renders canvas when name has been entered by the user
    
    Sends POST req to create user

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import Game from "./components/Game";
+import { connectWs } from "./ws";
+import { axiosInstance } from "../axiosInstance";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const socket = useRef(null);
+  const [name, setName] = useState(null);
+  const [input, setInput] = useState("");
 
-  return <div className="">Hello world</div>;
+  useEffect(() => {
+    socket.current = connectWs();
+  }, []);
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const res = await axiosInstance.post("/users", {
+      username: input,
+    });
+    setName(input);
+    console.log(res.data);
+  }
+  return (
+    <div className="">
+      {!name ? (
+        <div>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <label htmlFor="username">Enter your name</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Your name (e.g. John Doe)"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button type="submit">submit</button>
+          </form>
+        </div>
+      ) : (
+        <Game />
+      )}
+    </div>
+  );
 }
 
 export default App;
